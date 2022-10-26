17a3f910e77e08e6cdd7615ca6359803a5bfd168
Sárándi <sarandi.balint@gmail.com>
2022-10-26 15:03:53 +0200

    Idaid jutottam oran

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 "use strict";
-let userNamePattern = /^[a-z]{3,}$/;
-console.log(userNamePattern.test("bob"));
-console.log(userNamePattern.test("!@1"));
-console.log(userNamePattern.test("z"));
-//let emailSearchPattern = /[a-z.-_]+@[a-z.-_]+/g
-let emailSearchPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-let szoveg = "A petrik email címe: petrik@petrik.hu, a titkárságé: titkarsag@petrik.hu, valamint az info@example.hu";
+let userNamePattern = /^[a-z][A-Z]{1,}$/;
+let countryPattern = /^[a-z][A-Z]{1,}$/;
+let cityPattern = /^[a-z][A-Z]{1,}$/;
+let utcahszPattern = /^[a-z]{1,}[a-zA-Z0-9 ]*$/;
+let postcodePattern = /^[a-z][A-Z][0-9]{1,}$/;
+let bankcardPattern = /^[0-9]{4}[-]{1}[0-9]{4}[-]{1}[0-9]{4}[-]{1}[0-9]{4}$/;
+let validationnumberPattern = /^[0-9]{3}$/;
+let bankcardnamePattern = /^[A-Z]{1}[a-z]{1,}[ ]{1}[A-Z]{1}[a-z]{1,}$/;
+document.addEventListener('DOMContentLoaded', () => {
+    var _a;
+    (_a = document.getElementById('fizetes')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        //felhnev
+        if (!userNamePattern.test(document.getElementById('nev').value)) {
+            document.getElementById('nevlabel').style.color = "red";
+            console.log("rossznev");
+        }
+        else if (userNamePattern.test(document.getElementById('nev').value)) {
+            document.getElementById('nevlabel').style.color = "black";
+            console.log("jonevnev");
+        }
+        //orszag
+        if (!countryPattern.test(document.getElementById('orszag').value)) {
+            document.getElementById('orszaglabel').style.color = "red";
+        }
+        else if (countryPattern.test(document.getElementById('orszag').value)) {
+            document.getElementById('orszaglabel').style.color = "black";
+        }
+    });
+    if (userNamePattern.test(document.getElementById('nev').value) && countryPattern.test(document.getElementById('orszag').value) && cityPattern.test(document.getElementById('varos').value) && utcahszPattern.test(document.getElementById('varos').value) && postcodePattern.test(document.getElementById('irsz').value) && bankcardPattern.test(document.getElementById('kartyaszam').value) && validationnumberPattern.test(document.getElementById('kod').value) && bankcardnamePattern.test(document.getElementById('kartyanev').value)) {
+        const element = document.getElementById("form");
+        element.remove();
+    }
+});
+// console.log(emailSearchPattern.test("saf.adf@gmail.com"))
+//let emailSearchPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+/* let szoveg = "A petrik email címe: petrik@petrik.hu, a titkárságé: titkarsag@petrik.hu, valamint az info@example.hu"
 let emailCimek = szoveg.matchAll(emailSearchPattern);
-for (let email of emailCimek) {
-    console.log(email[0]);
-}
-/* console.log(emailSearchPattern.test("saf.adf@gmail.com"))
-console.log(emailSearchPattern.test("@gmail.com"))
-console.log(emailSearchPattern.test("saf.adfgmail.com")) */
+for(let email of emailCimek){
+    console.log(email[0])
+} */
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,52 @@
-let userNamePattern = /^[a-z]{3,}$/;
-console.log(userNamePattern.test("bob"));
-console.log(userNamePattern.test("!@1"));
-console.log(userNamePattern.test("z"));
+let userNamePattern = /^[a-z][A-Z]{1,}$/;
+let countryPattern = /^[a-z][A-Z]{1,}$/;
+let cityPattern = /^[a-z][A-Z]{1,}$/;
+let utcahszPattern = /^[a-z]{1,}[a-zA-Z0-9 ]*$/;
+let postcodePattern = /^[a-z][A-Z][0-9]{1,}$/;
+let bankcardPattern = /^[0-9]{4}[-]{1}[0-9]{4}[-]{1}[0-9]{4}[-]{1}[0-9]{4}$/;
+let validationnumberPattern = /^[0-9]{3}$/;
+let bankcardnamePattern = /^[A-Z]{1}[a-z]{1,}[ ]{1}[A-Z]{1}[a-z]{1,}$/;
 
 
-//let emailSearchPattern = /[a-z.-_]+@[a-z.-_]+/g
 
-let emailSearchPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
-let szoveg = "A petrik email címe: petrik@petrik.hu, a titkárságé: titkarsag@petrik.hu, valamint az info@example.hu"
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('fizetes')?.addEventListener('click', ()=>{
+        //felhnev
+        if(!userNamePattern.test((document.getElementById('nev') as HTMLInputElement).value)){
+            (document.getElementById('nevlabel') as HTMLElement).style.color = "red";
+            console.log("rossznev")
+        }else if(userNamePattern.test((document.getElementById('nev') as HTMLInputElement).value)){
+            (document.getElementById('nevlabel') as HTMLElement).style.color = "black";
+            console.log("jonevnev")
+        }
+
+        //orszag
+        if(!countryPattern.test((document.getElementById('orszag') as HTMLInputElement).value)){
+            (document.getElementById('orszaglabel') as HTMLElement).style.color = "red";
+        }else if(countryPattern.test((document.getElementById('orszag') as HTMLInputElement).value)){
+            (document.getElementById('orszaglabel') as HTMLElement).style.color = "black";
+        }
+    });
+
+    if(userNamePattern.test((document.getElementById('nev') as HTMLInputElement).value) && countryPattern.test((document.getElementById('orszag') as HTMLInputElement).value) && cityPattern.test((document.getElementById('varos') as HTMLInputElement).value) && utcahszPattern.test((document.getElementById('varos') as HTMLInputElement).value) && postcodePattern.test((document.getElementById('irsz') as HTMLInputElement).value) && bankcardPattern.test((document.getElementById('kartyaszam') as HTMLInputElement).value) && validationnumberPattern.test((document.getElementById('kod') as HTMLInputElement).value) && bankcardnamePattern.test((document.getElementById('kartyanev') as HTMLInputElement).value)){
+
+
+
+            const element = (document.getElementById("form") as HTMLElement) ;
+            element.remove();
+    }
+
+})
+
+// console.log(emailSearchPattern.test("saf.adf@gmail.com"))
+//let emailSearchPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+/* let szoveg = "A petrik email címe: petrik@petrik.hu, a titkárságé: titkarsag@petrik.hu, valamint az info@example.hu"
 let emailCimek = szoveg.matchAll(emailSearchPattern);
 for(let email of emailCimek){
     console.log(email[0])
-}
-
-
-/* console.log(emailSearchPattern.test("saf.adf@gmail.com"))
-console.log(emailSearchPattern.test("@gmail.com"))
-console.log(emailSearchPattern.test("saf.adfgmail.com")) */
-
+} */
 
 
 
